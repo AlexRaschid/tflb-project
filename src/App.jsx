5979f6c0f84534855d730e0dc6e6a9c8dfb54548
AlexRaschid <AlexRaschid@users.noreply.github.com>
2024-11-10 10:30:32 -0500

    +container page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Board from './components/board'
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Board></Board>
-    </>
+    //style padding cheap fix, use .css later
+    <Container fluid style={{ padding: 0 }}>
+      <Row>
+        <Col sm={2}>
+          <Button>Test</Button>
+        </Col>
+        <Col>
+          <Board></Board>
+        </Col>
+      </Row>
+    </Container>  
   )
 }
 
