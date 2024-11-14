0cfdf3605e0ef3f0e85676f7db15f3009543d052
AlexRaschid <AlexRaschid@users.noreply.github.com>
2024-11-14 07:14:31 -0500

    +template format and api prep

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,7 +1,24 @@
+//https://github.com/leonlarsson/the-finals-api
+//https://api.the-finals-leaderboard.com/#tag/leaderboards
+//example: https://api.the-finals-leaderboard.com/v1/leaderboard/s4sponsor/crossplay
 
-export default function player(){
+const BASE_URL = "https://api.the-finals-leaderboard.com";
+const S4_LB = "/v1/leaderboard/s4/";
+
+export default function Player(){
     return(
-        <></>
-
+        <tr>
+            <td>1</td>
+            <td>0</td>
+            <td>
+                <div>Otto#1111</div>
+                <div>0tt0</div>
+                <div>TottO</div>
+            </td>
+            <td>
+                <div>Ruby</div>
+                <div>80,000</div>
+            </td>
+        </tr>
     )
 }
