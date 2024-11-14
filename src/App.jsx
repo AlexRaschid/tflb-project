1ab51b079b8ed6c752c7f05ff56296c464049fbd
AlexRaschid <AlexRaschid@users.noreply.github.com>
2024-11-14 08:59:39 -0500

    +react query client provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 import './App.css'
-//import './components/board.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react'
-
-import Board from './components/Board.jsx'
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+import Board from './components/Board.jsx'
+
+const queryClient = new QueryClient();
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     //style padding cheap fix, use .css later
-    <Container>
-      <Row>
-        <Col>
-          <Board></Board>
-        </Col>
-      </Row>
-    </Container>  
+    <QueryClientProvider client = {queryClient}>
+      <Container>
+            <Row>
+              <Col>
+                <Board></Board>
+              </Col>
+            </Row>
+      </Container> 
+    </QueryClientProvider> 
   )
 }
 
