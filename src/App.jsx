a8022d462f055ebafdbbe34e2df17f0c9711f6d8
AlexRaschid <AlexRaschid@users.noreply.github.com>
2024-11-15 10:51:14 -0500

    +comment todo notes

--- src/App.jsx
+++ src/App.jsx
@@ -29,3 +29,29 @@ function App() {
 }
 
 export default App
+
+
+
+{/*
+    TODO: reorganize code into components:
+    FetchLeaderboard - makes main leaderboard api calls, returns data
+    PlayerBoardEntry - responsible for each row and cells of players from the api key
+        PlayerLeagueCurrent - handles the image, rank text, and rank number positioning in league category
+        PlayerName - name col
+
+        
+    TODO Features:
+        -Search Bar
+        -Show Select amt of player entries (10,25,50,100), and hide the rest
+            -Arrows to navigate pages of entries
+    
+    TODO: Design:
+        -Replace stacked usernames under name, with inline icons of the platforms,
+            reveal names w/ hover or something.
+
+        -Colors for 24h change?
+
+
+        
+
+*/}
