dd49b31e4e4cc29e8280fe270911b22b4dd3b24d
AlexRaschid <AlexRaschid@users.noreply.github.com>
2024-11-15 21:34:45 -0500

    +made into a component
    for organization

---
+++ src/components/BoardTable.jsx
@@ -0,0 +1,24 @@
+// LeaderboardTable.jsx
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import PlayerRow from './PlayerRow';
+
+export default function LeaderboardTable({ players }) {
+    return (
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>Rank</th>
+                    <th>24h</th>
+                    <th>Name</th>
+                    <th>League</th>
+                </tr>
+            </thead>
+            <tbody>
+                {players.map((player, index) => (
+                    <PlayerRow key={index} player={player} />
+                ))}
+            </tbody>
+        </Table>
+    );
+}
