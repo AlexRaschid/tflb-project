7ff9ddf8f341ebed97a3975cb012228904560ce8
AlexRaschid <AlexRaschid@users.noreply.github.com>
2024-11-16 08:00:27 -0500

    renamed BoardHeader to BoardSearch

---
+++ src/components/BoardSearch.jsx
@@ -0,0 +1,27 @@
+import { Stack, Form, Button } from "react-bootstrap";
+import Dropdown from 'react-bootstrap/Dropdown';
+
+export default function BoardSearch(){
+    return(
+        <Stack direction="horizontal" gap={2} className="align-items-center justify-content-center">
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    s4 (current)
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">s3 (TODO)</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">s2 (TODO)</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+            <Form>
+                <Form.Control className="me-auto" placeholder="Search player names..." />
+            </Form>
+        </Stack>
+    );
+}
+
+{/*
+            <Form.Control className="me-auto" placeholder="Search player names..." />
+            <Button variant="primary">Search</Button>    
+*/}
